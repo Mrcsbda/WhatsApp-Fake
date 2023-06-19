488bcd923035341baef9f5e104e95addadbc58cd
mrcsbda <macbedoya@gmail.com>
2023-06-19 17:16:19 -0500

    :construction: current user

--- src/app/scripts/UI/signIn/loginValidation.js
+++ src/app/scripts/UI/signIn/loginValidation.js
@@ -3,40 +3,45 @@ import { getUsers } from "../../services/getUsers"
 const userInput = document.getElementById('user');
 const passwordInput = document.getElementById('password');
 const form = document.getElementById('form');
+const loginContainer = document.querySelector('.main-sign-in');
+const homeContainer = document.querySelector('.main')
 
 export const loginValidation = async () => {
     const data = await getUsers()
     console.log(data)
     form.addEventListener("submit", (event)=> {
         event.preventDefault() 
-        const userFound = data.filter(user => user.phone === userInput.value)
- 
+        const userFound = data.find(user => user.phone === userInput.value)
         if(userInput.value === "" || passwordInput.value === "" ) {
             Swal.fire({
                 icon: 'error',
                 title: 'Oops...',
                 text: 'Tienes campos sin rellenar',
               })
-        } else if (!userFound.length) {
+        } else if (!userFound) {
             Swal.fire({
                 icon: 'error',
                 title: 'Oops...',
                 text: 'El usuario ingresado no existe',
               })
-        } else if (passwordInput.value ==! userFound[0].password) {
+        } else if (passwordInput.value !== userFound.password) {
             Swal.fire({
                 icon: 'error',
                 title: 'Oops...',
                 text: 'Contraseña incorrecta, vuelve a intentarlo',
               })
         } else {
-            
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: `Bienvenido/a ${userFound.name}`,
+                showConfirmButton: false,
+                timer: 3000
+              })
+            loginContainer.classList.remove('login-active');
+            homeContainer.classList.add('home-active');
+            localStorage.setItem("userCurrentState","home");
+            localStorage.setItem("currentUser",JSON.stringify(userFound));
         }
-
-
-
     })
-}
-const formValidation = () => {
-
 }
--- src/app/scripts/main.js
+++ src/app/scripts/main.js
@@ -1,6 +1,26 @@
 import "../styles/styles.scss"
 import {activeViews} from './UI/home/btnsActiveViews.js'
-import { loginValidation } from "./UI/signIn/validationUser"
+import { loginValidation } from "./UI/signIn/loginValidation.js"
+const userCurrentState = localStorage.getItem("userCurrentState");
+const loginContainer = document.querySelector('.main-sign-in');
+const homeContainer = document.querySelector('.main');
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    switch(true) {
+        case !userCurrentState:
+            loginContainer.classList.add('login-active')
+            homeContainer.classList.remove('home-active')
+            break;
+        case userCurrentState === "home":
+            loginContainer.classList.remove('login-active')
+            homeContainer.classList.add('home-active')
+            break;
+        default:
+            loginContainer.classList.remove('login-active')
+            homeContainer.classList.remove('home-active')
+            break;
+    }
+})
 
 activeViews()
 loginValidation()
